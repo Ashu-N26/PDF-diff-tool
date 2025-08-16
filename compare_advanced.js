64638581a4179c842d4d0dc7a9dd469215e071ed
Ashu-N26 <ashutoshnanaware26@gmail.com>
2025-08-16 12:21:26 +0530

    Update compare_advanced.js

--- compare_advanced.js
+++ compare_advanced.js
@@ -1,59 +1,40 @@
 const fs = require("fs");
 const { PDFDocument, rgb } = require("pdf-lib");
-const pdfParse = require("pdf-parse");
+const diffNative = require("./diff_native/diff");
 
 async function compareAdvanced(pdfPath1, pdfPath2) {
-  const pdf1Buffer = fs.readFileSync(pdfPath1);
-  const pdf2Buffer = fs.readFileSync(pdfPath2);
-
-  const pdf1Text = (await pdfParse(pdf1Buffer)).text;
-  const pdf2Text = (await pdfParse(pdf2Buffer)).text;
-
   const pdfDoc = await PDFDocument.create();
   const [doc1, doc2] = await Promise.all([
-    PDFDocument.load(pdf1Buffer),
-    PDFDocument.load(pdf2Buffer),
+    PDFDocument.load(fs.readFileSync(pdfPath1)),
+    PDFDocument.load(fs.readFileSync(pdfPath2)),
   ]);
 
-  const page1 = await pdfDoc.copyPages(doc1, [0]);
-  const page2 = await pdfDoc.copyPages(doc2, [0]);
+  // Copy first pages from both PDFs
+  const [page1] = await pdfDoc.copyPages(doc1, [0]);
+  const [page2] = await pdfDoc.copyPages(doc2, [0]);
+  pdfDoc.addPage(page1);
+  pdfDoc.addPage(page2);
 
-  pdfDoc.addPage(page1[0]);
-  pdfDoc.addPage(page2[0]);
-
-  const comparisonPage = pdfDoc.addPage([600, 800]);
-  comparisonPage.drawText("Differences Detected:", {
-    x: 50,
-    y: 750,
-    size: 18,
-    color: rgb(1, 0, 0),
+  // Add summary page
+  const summaryPage = pdfDoc.addPage([600, 800]);
+  summaryPage.drawText("🔎 Differences Detected", {
+    x: 50, y: 750, size: 18, color: rgb(1, 0, 0),
   });
 
-  let differences = [];
-  const lines1 = pdf1Text.split("\n");
-  const lines2 = pdf2Text.split("\n");
-
-  lines1.forEach((line, i) => {
-    if (line !== lines2[i]) {
-      differences.push(`Line ${i + 1}: "${line}"  ⟶  "${lines2[i]}"`);
-    }
-  });
-
-  differences.slice(0, 25).forEach((diff, i) => {
-    comparisonPage.drawText(diff, {
-      x: 50,
-      y: 720 - i * 20,
-      size: 10,
-      color: rgb(0, 0, 0),
-    });
+  const diffs = await diffNative(pdfPath1, pdfPath2);
+  diffs.slice(0, 25).forEach((diff, i) => {
+    summaryPage.drawText(
+      `Line ${diff.line}: "${diff.old}" ⟶ "${diff.new}"`,
+      { x: 50, y: 720 - i * 20, size: 10, color: rgb(0, 0, 0) }
+    );
   });
 
   const resultPath = `comparison_${Date.now()}.pdf`;
   const pdfBytes = await pdfDoc.save();
   fs.writeFileSync(resultPath, pdfBytes);
-
   return resultPath;
 }
 
 module.exports = compareAdvanced;
+
 
